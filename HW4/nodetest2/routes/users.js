d2478bc517d2b783db1de01d40755af12b0c9833
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2018-05-04 10:27:35 -0700

    Kurt

---
+++ HW4/nodetest2/routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+ //GET users listing. 
+/* router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+}); */
+/* GET userlist. */router.get('/userlist',function(req, res){ 
+  var db = req.db;
+   var collection = db.get('userlist');
+   collection.find({},{},function(e,docs){    res.json(docs);
+  });
+  //var docs = [{'username' : 'test1','email' : 'test1@test.com'}]
+    //,'fullname' : 'Bob Smith','age' : 27,'location' : 'San Francisco','gender' : 'Male'};
+ res.json(docs)
+});
+
+module.exports = router;
